bde8fd5e3669c8fad31d4a20517145060e4d0e7d
salvo <salvo.cuccurullo@gmailcom>
2018-10-17 22:47:03 +0200

    Adding created, updated to TvShow

--- StiCazzi/models.py
+++ StiCazzi/models.py
@@ -85,6 +85,8 @@ class TvShow(models.Model):
     vote = models.DecimalField(max_digits=4, decimal_places=2, default=5)
     poster = models.CharField(max_length=300, default='')
     datetime = models.DateTimeField(auto_now_add=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
