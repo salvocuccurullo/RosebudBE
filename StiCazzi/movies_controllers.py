8641bdead20fb951973c1ed0c1a08f0afe1ad544
salvo <salvo.cuccurullo@gmailcom>
2018-10-18 15:17:34 +0200

    lazy load 04

--- StiCazzi/movies_controllers.py
+++ StiCazzi/movies_controllers.py
@@ -95,6 +95,8 @@ def get_tvshows_new(request):
     has_more = True
     if len(movie_list) <= upper_bound:
         has_more = False
+    if lazy_load:
+        has_more = False
 
     print("List size: " + str(len(bounded)))
     print("Has more: " + str(has_more))
