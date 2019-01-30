389841d972bd05be7b6b64f981e9bc3bbc82b4e4
Salvatore Cuccurullo <salvo.cuccurullo@gmail.com>
2019-01-30 15:13:46 +0100

    Fix "show more" when query string is smaller than 4 #4

--- StiCazzi/movies_controllers.py
+++ StiCazzi/movies_controllers.py
@@ -46,7 +46,7 @@ def get_tvshows_new_opt(request):
     logger.debug("Get Tvshows news opt called")
     logger.debug("Current page: %s", current_page) 
 
-    if len(query) < 4 and int(current_page) == 1:
+    if query and len(query) < 4 and int(current_page) == 1:
         logger.debug("Query too short: %s", query)
         response['result'] = 'failure'
         response['message'] = 'Query String too short'
