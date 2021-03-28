13f4745a5824f4186b529cd7f3b23556bdc6d16e
sa.cuccurullo@douglas.de <sa.cuccurullo@douglas.de>
2021-03-28 17:07:17 +0200

    adding env var MONGO_API_2ND_DB_URL to env.py

--- StiCazzi/env.py
+++ StiCazzi/env.py
@@ -1,6 +1,7 @@
 import os
 
 MONGO_API_URL = os.environ.get('MONGO_API_URL', '')
+MONGO_API_2ND_DB_URL = os.environ.get('MONGO_API_2ND_DB_URL', '')
 MONGO_API_USER = os.environ.get('COVER_API_USER', '')
 MONGO_API_PWD = os.environ.get('COVER_API_PW', '')
 MONGO_SERVER_CERTIFICATE = os.environ.get('MONGO_SERVER_CERTIFICATE')
