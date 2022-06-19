caef0f85ff744832e3fdd38be1dd9d12e341f6aa
sa.cuccurullo@douglas.de <sa.cuccurullo@douglas.de>
2022-06-19 10:54:45 +0200

    increasing retrieved movies default limit to 40

--- StiCazzi/movies_controllers.py
+++ StiCazzi/movies_controllers.py
@@ -30,7 +30,7 @@ def get_tvshows_new_opt(request):
         username = i_data.get('username', '')
         kanazzi = i_data.get('kanazzi', '')
         query = i_data.get('query', '')
-        limit = i_data.get('limit', 15)
+        limit = i_data.get('limit', 40)
         current_page = i_data.get('current_page', 1)
         lazy_load = i_data.get('lazy_load', True)
     except (TypeError, ValueError):
