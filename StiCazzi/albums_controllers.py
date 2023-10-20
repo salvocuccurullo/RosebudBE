e23c35dd84edfdc84bf97f14ec8f744fb429594d
Salvatore Cuccurullo <sa.cuccurullo@douglas.de>
2023-10-20 11:52:55 +0200

    adding get_one_track method

--- StiCazzi/albums_controllers.py
+++ StiCazzi/albums_controllers.py
@@ -124,6 +124,48 @@ def set_one_album(request):
     return response
 
 @authentication
+def get_one_track(request):
+    """ Get one track from Mongo """
+    logger.debug("get one track pymnongo called")
+    response = {}
+
+    try:
+        i_data = json.loads(request.body)
+        doc_id = i_data.get('doc_id', '')
+    except (TypeError, ValueError):
+        response['result'] = 'failure'
+        response['message'] = 'Bad input format'
+        response['status_code'] = 400
+        return response
+
+    client = MongoClient()
+    client = MongoClient(os.environ['MONGO_SERVER_URL_PYMONGO'])
+
+    db = client.rosebud_dev
+    coll = db.song
+
+    try:
+        res = coll.find_one({"_id": ObjectId(doc_id)})
+        if res:
+            response['song'] = \
+            {
+                "id": str(res['_id']),
+                "title": res['name'],
+                "author": res['author'],
+                "location": res['location'],
+                "thumbnail": res['thumbnail'],
+                "spotifyAlbumUrl": res['spotifyAlbumUrl'],
+                "release_date": res.get('release_date', ''),
+                "remarkable": res.get('remarkable', '')
+            }
+    except Exception as e:
+        response['result'] = 'failure'
+        response['message'] = str(e)
+        response['status_code'] = 400
+
+    return response
+
+@authentication
 def set_one_track(request):
     """ Set one track on Mongo """
     logger.debug("set one track pymnongo called")
