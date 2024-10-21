b01f15c1ac17419f5e02bacaf7a1c63f5b88a532
Salvatore Cuccurullo <sa.cuccurullo@douglas.de>
2024-10-21 17:25:49 +0200

    testing raw strings on regex for avoiding syntax warnings - one more

--- StiCazzi/albums_controllers.py
+++ StiCazzi/albums_controllers.py
@@ -698,7 +698,7 @@ def get_tracks(request):
     elif special and special in ('today', 'yesterday', 'tomorrow', 'to_date'):
         mongo_stmt = { "$and": [
                         {"release_date": { "$exists": True }, "$expr": { "$gt": [{ "$strLenCP": '$release_date' }, 7] } },#release date exists and its lenght > 7 (full date)
-                        {"release_date": { "$regex": ".*\-%s\-%s" % (month, day)}}
+                        {"release_date": { "$regex": r".*\-%s\-%s" % (month, day)}}
                     ]}
     else:
         response['result'] = 'failure'
